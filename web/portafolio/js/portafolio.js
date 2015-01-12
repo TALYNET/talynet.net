4f61ecf58e26234e6250f30eee091cf42bda0334
cyberiousFX <cyberiousFX@192.168.50.6>
2015-01-12 00:38:17 -0300

    Con portafolio.

---
+++ web/portafolio/js/portafolio.js
@@ -0,0 +1,58 @@
+// JavaScript Document
+
+		$(document).ready(function() {
+			$("#art1").click(function() {
+				window.open('http://cief.cl', '_blank');
+			});
+			$("#art2").click(function() {
+				window.open('http://devouement.cl', '_blank');
+			});
+			$("#art3").click(function() {
+				window.open('http://editorialcrece.cl', '_blank');
+			});
+			$("#art4").click(function() {
+				window.open('http://g-talent.cl', '_blank');
+			});
+			$("#art5").click(function() {
+				window.open('http://inge-red.cl', '_blank');
+			});
+
+			$("#art6").click(function() {
+				window.open('http://jardininfantilsanbenito.cl', '_blank');
+			});
+
+			$("#art7").click(function() {
+				window.open('http://mipymeinnova.com', '_blank');
+			});
+
+			$("#art8").click(function() {
+				window.open('http://mundopipa.cl', '_blank');
+			});
+
+			$("#art9").click(function() {
+				window.open('http://sindicatodoselvolcan.cl', '_blank');
+			});
+
+			$("#art10").click(function() {
+				window.open('http://sitenet.cl', '_blank');
+			});
+
+
+			$("#art11").click(function() {
+				window.open('http://soluciones-it.cl', '_blank');
+			});
+
+			$("#art12").click(function() {
+				window.open('http://tecuida.cl', '_blank');
+			});
+
+			$("#art13").click(function() {
+				window.open('http://vincipromociones.cl', '_blank');
+			});
+
+			$("#art14").click(function() {
+				window.open('http://reporterodigital.cl', '_blank');
+			});
+
+
+		});
