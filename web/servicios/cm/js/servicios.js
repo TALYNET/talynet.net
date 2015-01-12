d5d92bb227c765035c90a5854a2376351afa2835
cyberiousFX <cyberiousFX@192.168.50.6>
2015-01-12 00:22:06 -0300

    uno

---
+++ web/servicios/cm/js/servicios.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+/*
+		$(document).ready(function() {
+			$("#art1").click(function() {
+				window.open('http://www.talynet.net/hosting/contactanos/', '_blank');
+			});
+
+		});
+*/
